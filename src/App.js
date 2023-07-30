00597f6a154aa90094538b4ed30a59968d3e59c7
LazY <darshitmistry2@gmail.com>
2023-07-30 09:22:57 +0530

    Searched Functionality was added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Pages from "./pages/Pages";
 import Categouries from "./components/Categouries";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+      <Search/>
         <Categouries />
         <Pages />
       </BrowserRouter>
---
+++ src/pages/Searched.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components'
+
+const Searched = () => {
+  const params = useParams();
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=337a49691f70489ba992a090c19ff28d&query=${name}`
+    );
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results);
+  };
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+  return (
+  <Grid>
+    {searchedRecipes.map((item)=>{
+      return(
+        <Card id={item.id}>
+          <img src={item.image} alt="" />
+          <h4>{item.title}</h4>
+        </Card>
+      )
+    })}
+  </Grid>
+  )
+};
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 2fr));
+  grid-gap: 3rem;
+`;
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+export default Searched;
