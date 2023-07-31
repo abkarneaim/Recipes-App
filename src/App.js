c4d656ff8bc3cef634db1c626c40cf1283544702
LazY <darshitmistry2@gmail.com>
2023-07-31 08:53:31 +0530

    cuisin section was updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-      <Search/>
+        <Search />
         <Categouries />
         <Pages />
       </BrowserRouter>
